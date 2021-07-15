3a67baeca06047b1dd159d48f01325a3a4d3ba8f
Hibrael01 <hibrael46@gmail.com>
2021-07-15 18:46:58 -0300

    Finalizando comunidades e iniciando a implementacao de mensagens

---
+++ pages/api/mensagens.js
@@ -0,0 +1,28 @@
+import {SiteClient} from 'datocms-client'
+
+export default async function recebeRequests(request, response){
+    if(request.method === 'POST'){
+        const TOKEN= 'e0e2123546e5339c4b2d6e7c40455a';
+
+        const client = new SiteClient(TOKEN);
+
+        const createdRecord = await client.items.create({
+            itemType: "972362",
+            ...request.body,
+            /*title: "Colheita Feliz",
+            imageUrl: "https://pbs.twimg.com/profile_images/1130326351/colheita_400x400.jpg",
+            creator_slug: "Hibrael"*/
+        })
+
+        console.log(createdRecord);
+        response.json({
+            dados: 'Um dado qualquer', 
+            createdRecord : createdRecord,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, somente no POST!'
+    })
+}
--- pages/index.js
+++ pages/index.js
@@ -56,24 +56,74 @@ export default function Home() {
   const gitHubUser = 'Hibrael01'
   const [comunidades, setComunidades] = React.useState([]);
 
-  /*const comunidades = [
-      'Alurakut'
-  ]*/
+  const [mensagens, setMensagens] = React.useState([]);
 
-  const pessoasFavoritas = ['rafaballerini', 'omariosouto', 'juunegreiros', 'felipefialho', 'gustavoguanabara', 'marcobrunodev']
+  const pessoasFavoritas = ['rafaballerini', 'omariosouto', 'juunegreiros', 'felipefialho', 'gustavoguanabara', 'marcobrunodev', 'Hibrael01']
   
   //Pegar array de dados do github
   const [seguidores, setSeguidores] = React.useState([]);
   React.useEffect(function(){
-
+     
+    
+    //Utiliza GET
     fetch('https://api.github.com/users/peas/followers').then(function (serverResponse){
-
       return serverResponse.json()
     }).then(function (completeResponse){
       setSeguidores(completeResponse)
     })
 
+    //API GraphQL
+    fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Authorization' : 'e13cacd80a8ac0ad5859f564e5b448',
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+      },
+      body: JSON.stringify({"query": `query {
+        allCommunities{
+          id
+          title
+          imageUrl
+          creatorSlug
+        }
+      }`})
+
+    })
+    .then((response) => response.json())
+    .then((respostaCompleta) => {
+      const comunidadesDoDB = respostaCompleta.data.allCommunities;
+      console.log(comunidadesDoDB)
+      setComunidades(comunidadesDoDB)
+    })
+
+  //Setando Mensagens
+  fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Authorization' : 'e13cacd80a8ac0ad5859f564e5b448',
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+      },
+      body: JSON.stringify({"query": `query {
+        allMessages{
+          id
+          name
+          yourmessage
+        }
+      }`})
+
+    })
+    .then((response) => response.json())
+    .then((respostaCompleta) => {
+      const mensagensNoDb = respostaCompleta.data.allMessages;
+      console.log(mensagensNoDb)
+      setMensagens(mensagensNoDb)
+    })
+
   }, [])
+
+  
 
 
   return (
@@ -105,21 +155,36 @@ export default function Home() {
 
             const dataForm = new FormData(e.target);
             console.log(dataForm.get('title'))
+            console.log(dataForm.get('imageUrl'))
 
             const comunidade = {
-              id: new Date().toISOString,
               title: dataForm.get('title'),
-              image: dataForm.get('image'),
+              imageUrl: dataForm.get('imageUrl'),
+              creatorSlug: gitHubUser,
             }
 
             if(dataForm.get('title') == ''){
               alert('Insira um nome para a comunidade')
             }else{
-              const novasComunidades = [...comunidades, comunidade]
-              setComunidades(novasComunidades)
+
+              fetch('/api/comunidades', {
+                method : 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify(comunidade)
+              })
+              .then(async (response) => {
+                const dados = await response.json();
+                console.log(dados.createdRecord);
+                const comunidade = dados.createdRecord
+                console.log(comunidade)
+                const novasComunidades = [...comunidades, comunidade]
+                setComunidades(novasComunidades)
+              })
+             
             }  
             
-
           }}>
 
             <div>
@@ -129,16 +194,89 @@ export default function Home() {
               aria-label="Qual vai ser o nome da sua comunidade?">
               </input>
               <input placeholder="Qual a URL da imagem de sua comunidade?"
-              name="image"
+              name="imageUrl"
               aria-label="Qual a URL da imagem de sua comunidade?">
               </input>
 
             </div>
             <button>Criar comunidade</button>
             
+            <hr/>
           </form>
-        </Box>
 
+          
+          <form onSubmit={function createMessage(e){
+            e.preventDefault();
+
+            const dataForm = new FormData(e.target);
+            console.log(dataForm.get('name'))
+            console.log(dataForm.get('message'))
+
+            const mensagem = {
+              name: dataForm.get('name'),
+              yourmessage: dataForm.get('message'),
+            }
+
+            if(dataForm.get('name') == ''){
+              alert('Digite seu nome')
+            }else{
+
+              fetch('/api/mensagens', {
+                method : 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify(mensagem)
+              })
+              .then(async (response) => {
+                const dados = await response.json();
+                console.log(dados.createdRecord);
+                const mensagem = dados.createdRecord
+                console.log(mensagem)
+                const novaMensagem = [...mensagens, mensagem]
+                setMensagens(novaMensagem)
+              })
+             
+            }  
+            
+          }}>
+
+            <div>
+
+              <input placeholder="Digite seu nome aqui:"
+              name="name"
+              aria-label="Digite seu nome aqui">
+              </input>
+              <input placeholder="Digite sua mensagem:"
+              name="message"
+              aria-label="Digite sua mensagem">
+              </input>
+
+            </div>
+            <button>Deixar sua mensagem</button>
+            
+            <hr/><hr/>
+          </form>
+
+
+          <ProfileRelationsBoxWrapper>
+          <p>Mensagens: ({mensagens.length})</p>
+
+          <ul>
+            {mensagens.map((mensagemAtual) => {
+                return(
+                  <div key={mensagemAtual.id}>
+                      <span>{mensagemAtual.name} </span>
+                      <p>{mensagemAtual.yourmessage}</p>
+                  </div>
+                )
+            })}
+          </ul>
+
+          <a className="boxLink" href="#">Ver Mais</a>
+        </ProfileRelationsBoxWrapper>
+
+        </Box>   
       </div> 
 
       <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
@@ -150,29 +288,32 @@ export default function Home() {
           <p>Pessoas Favoritas Dev: ({pessoasFavoritas.length})</p>
 
           <ul>
-            {pessoasFavoritas.map((gitHubUser) => {
+
+            {pessoasFavoritas.slice(0,6).map((gitHubUser) => {  
                   return(
                     <li key={gitHubUser}>
-                      <a href={`/users/${gitHubUser}`} key={gitHubUser}>
+                      <a href={`https://github.com/${gitHubUser}`} key={gitHubUser}>
                         <img src={`https://github.com/${gitHubUser}.png`} />
                         <span>{gitHubUser}</span>
                       </a>
                     </li>
                   )
+                 
                 
             })}
           </ul>
+          <a className="boxLink" href="#">Ver Mais</a>
         </ProfileRelationsBoxWrapper>
 
         <ProfileRelationsBoxWrapper>
           <p>Comunidades: ({comunidades.length})</p>
 
           <ul>
-            {comunidades.map((comunidadeAtual) => {
+            {comunidades.slice(0,6).map((comunidadeAtual) => {
                 return(
                   <li key={comunidadeAtual.id}>
-                    <a href={`/users/${comunidadeAtual.title}`}>
-                      <img src={comunidadeAtual.image} />
+                    <a href={`/comunidades/${comunidadeAtual.id}`}>
+                      <img src={comunidadeAtual.imageUrl} />
                       <span>{comunidadeAtual.title}</span>
                     </a>
                   </li>
@@ -180,6 +321,7 @@ export default function Home() {
             })}
           </ul>
 
+          <a className="boxLink" href="#">Ver Mais</a>
         </ProfileRelationsBoxWrapper>
 
         
